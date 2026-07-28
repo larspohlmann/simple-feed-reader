d7bf5517fed96804a24020a3b0a73139c4ba8802
Lars Pohlmann <lars.pohlmann@googlemail.com>
2026-07-28 15:48:10 +0200

    feat(mail): compose the admin pending-approval notice (#164)

--- backend/src/Service/Mail/AccountMailer.php
+++ backend/src/Service/Mail/AccountMailer.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Service\Mail;
 
+use App\Dto\Mail\PendingApprovalNotice;
 use App\Entity\User;
+use App\Enum\RegistrationMethod;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
@@ -45,9 +47,30 @@ final readonly class AccountMailer
         $this->send($user, 'reset', ['%link%' => $this->link('/reset-password', $plainToken)]);
     }
 
+    public function sendPendingApprovalNotice(User $admin, PendingApprovalNotice $notice): void
+    {
+        $this->send($admin, 'admin_pending_approval', [
+            '%applicant_email%' => $notice->applicantEmail,
+            '%method%' => $this->methodLabel($notice, $admin->getLocale()),
+            '%pending_count%' => (string) $notice->pendingApprovalCount,
+            '%review_url%' => $notice->reviewUrl,
+        ]);
+    }
+
     private function link(string $path, string $plainToken): string
     {
         return rtrim($this->frontendUrl, '/') . $path . '?token=' . rawurlencode($plainToken);
+    }
+
+    private function methodLabel(PendingApprovalNotice $notice, string $locale): string
+    {
+        if (RegistrationMethod::OAuth === $notice->method) {
+            \assert(null !== $notice->oauthProvider);
+
+            return ucfirst($notice->oauthProvider);
+        }
+
+        return $this->translator->trans('admin_pending_approval.method_email_password', [], 'emails', $locale);
     }
 
     /** @param array<string, string> $params */
