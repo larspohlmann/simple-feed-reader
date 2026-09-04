809ffa2559691d70dbafa29c28ba14fc9e783bd0
Lars Pohlmann <lars.pohlmann@googlemail.com>
2026-09-04 13:30:48 +0200

    test(#834): name the unreadable-password transport failure

--- backend/tests/Service/Mail/Transport/DynamicMailTransportTest.php
+++ backend/tests/Service/Mail/Transport/DynamicMailTransportTest.php
@@ -71,6 +71,7 @@ final class DynamicMailTransportTest extends KernelTestCase
         $em->flush();
 
         $this->expectException(TransportException::class);
+        $this->expectExceptionMessage('The stored mail password is unreadable: Stored mail secret is not valid base64.');
         self::getContainer()->get(DynamicMailTransport::class)->activeTransport();
     }
 
